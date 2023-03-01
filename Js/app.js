26bbff65f60b589d837114dd1ec9bc2700de1958
NurulKarim053 <118825130+NurulKarim053@users.noreply.github.com>
2023-03-01 18:34:50 +0600

    Add files via upload

---
+++ Js/app.js
@@ -0,0 +1,13 @@
+let button = document.querySelector(`nav .button`)
+let sidebar = document.querySelector(`.sidebar`)
+
+function sidebarToggler(){
+    sidebar.classList.toggle(`active`)
+    if(sidebar.classList.contains(`active`) == true){
+    button.innerHTML = `<i class="bi bi-x"></i>`}
+    else{
+        button.innerHTML = `<i class="bi bi-list"></i>`
+    }
+}
+
+button.addEventListener(`click`,sidebarToggler)
